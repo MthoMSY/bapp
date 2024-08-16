2fb9d433c52581a1e12b03aeb682e95bb4bed758
MthoMSY <mthokozisi.myeza@chillisoft.co.za>
2024-08-16 09:42:03 +0200

    add disclaimer message constant

---
+++ src/assets/common-messages.ts
@@ -0,0 +1,4 @@
+export const DISCLAIMER =
+  "Any information on this website does not constitute as financial, tax, legal, \
+  investment, or other advice. Before making any decision or taking any action \
+  regarding your finances, you should consult a qualified Financial Advisor.";
