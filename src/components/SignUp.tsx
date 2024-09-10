12ff85b5cc4e5527384bcf97d3bbfd9441c10067
MthoMSY <mthokozisi.myeza@chillisoft.co.za>
2024-09-10 18:02:57 +0200

    improve sign up responsiveness

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -4,6 +4,7 @@ import { signUp } from "../features/user/userSlice";
 import { useNavigate } from "react-router-dom";
 import { useAppToast } from "../hooks/useAppToast";
 import { getIsLoadingClassName } from "./utils";
+import "./SignUp.css";
 
 type FormValues = {
   username: string;
@@ -47,129 +48,131 @@ export function SignUp() {
   const { errors } = formState;
 
   return (
-    <div className="box ">
-      <div className="block">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="field">
-            <div className="control has-icons-left">
-              <input
-                className="input is-rounded"
-                type="text"
-                placeholder="Username"
-                id="username"
-                {...register("username", {
-                  required: {
-                    message: "Username is required",
-                    value: true,
-                  },
-                  validate: {
-                    minLength: (fieldValue: string) => {
-                      return (
-                        minLengthValidation(fieldValue, 3) ||
-                        "Username length should be at least 3 characters long"
-                      );
+    <div className="section">
+      <div className="box">
+        <div className="block">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="field">
+              <div className="control has-icons-left">
+                <input
+                  className="input is-rounded"
+                  type="text"
+                  placeholder="Username"
+                  id="username"
+                  {...register("username", {
+                    required: {
+                      message: "Username is required",
+                      value: true,
                     },
-                    maxLength: (fieldValue: string) => {
-                      return (
-                        maxLengthValidation(fieldValue) ||
-                        "Password length must be less than 21 characters"
-                      );
+                    validate: {
+                      minLength: (fieldValue: string) => {
+                        return (
+                          minLengthValidation(fieldValue, 3) ||
+                          "Username length should be at least 3 characters long"
+                        );
+                      },
+                      maxLength: (fieldValue: string) => {
+                        return (
+                          maxLengthValidation(fieldValue) ||
+                          "Password length must be less than 21 characters"
+                        );
+                      },
                     },
-                  },
-                })}
-              />
-              <span className="icon is-small is-left">
-                <i className="fas fa-user"></i>
-              </span>
+                  })}
+                />
+                <span className="icon is-small is-left">
+                  <i className="fas fa-user"></i>
+                </span>
+              </div>
+              <p className="help is-danger">{errors.username?.message}</p>
             </div>
-            <p className="help is-danger">{errors.username?.message}</p>
-          </div>
-          <div className="field">
-            <div className="control has-icons-left">
-              <input
-                className="input is-rounded"
-                type="password"
-                placeholder="Password"
-                id="password"
-                {...register("password", {
-                  required: {
-                    message: "Password is required",
-                    value: true,
-                  },
-                  validate: {
-                    minLength: (fieldValue: string) => {
-                      return (
-                        minLengthValidation(fieldValue, 8) ||
-                        "Password length should be at least 8 characters long"
-                      );
+            <div className="field">
+              <div className="control has-icons-left">
+                <input
+                  className="input is-rounded"
+                  type="password"
+                  placeholder="Password"
+                  id="password"
+                  {...register("password", {
+                    required: {
+                      message: "Password is required",
+                      value: true,
                     },
-                    maxLength: (fieldValue: string) => {
-                      return (
-                        maxLengthValidation(fieldValue) ||
-                        "Password length must be less than 21 characters"
-                      );
+                    validate: {
+                      minLength: (fieldValue: string) => {
+                        return (
+                          minLengthValidation(fieldValue, 8) ||
+                          "Password length should be at least 8 characters long"
+                        );
+                      },
+                      maxLength: (fieldValue: string) => {
+                        return (
+                          maxLengthValidation(fieldValue) ||
+                          "Password length must be less than 21 characters"
+                        );
+                      },
                     },
-                  },
-                })}
-              />
-              <span className="icon is-small is-left">
-                <i className="fas fa-key"></i>
-              </span>
+                  })}
+                />
+                <span className="icon is-small is-left">
+                  <i className="fas fa-key"></i>
+                </span>
+              </div>
+              <p className="help is-danger">{errors.password?.message}</p>
             </div>
-            <p className="help is-danger">{errors.password?.message}</p>
-          </div>
-          <div className="field">
-            <div className="control has-icons-left">
-              <input
-                className="input is-rounded"
-                type="password"
-                placeholder="Confirm Password"
-                id="confirmedPassword"
-                {...register("confirmedPassword", {
-                  required: {
-                    message: "Field is required",
-                    value: true,
-                  },
-                  validate: {
-                    matchesPasswordField: (fieldValue: string) => {
-                      return (
-                        fieldValue === getValues("password") ||
-                        "Passwords do not match"
-                      );
+            <div className="field">
+              <div className="control has-icons-left">
+                <input
+                  className="input is-rounded"
+                  type="password"
+                  placeholder="Confirm Password"
+                  id="confirmedPassword"
+                  {...register("confirmedPassword", {
+                    required: {
+                      message: "Field is required",
+                      value: true,
                     },
-                  },
-                })}
-              />
-              <span className="icon is-small is-left">
-                <i className="far fa-check-square"></i>
-              </span>
+                    validate: {
+                      matchesPasswordField: (fieldValue: string) => {
+                        return (
+                          fieldValue === getValues("password") ||
+                          "Passwords do not match"
+                        );
+                      },
+                    },
+                  })}
+                />
+                <span className="icon is-small is-left">
+                  <i className="far fa-check-square"></i>
+                </span>
+              </div>
+              <p className="help is-danger">
+                {errors.confirmedPassword?.message}
+              </p>
             </div>
-            <p className="help is-danger">
-              {errors.confirmedPassword?.message}
-            </p>
-          </div>
+            <div className="field">
+              <div className="buttons is-centered">
+                <button
+                  type="submit"
+                  className={`button is-white ${getIsLoadingClassName(loading)}`}
+                  id="login"
+                >
+                  SignUp
+                </button>
+              </div>
+            </div>
+          </form>
           <div className="field">
-            <div className="buttons is-centered">
+            <div className="buttons is-right">
               <button
-                type="submit"
-                className={`button is-white ${getIsLoadingClassName(loading)}`}
-                id="login"
+                className="button is-ghost"
+                onClick={() => {
+                  navigate("/login");
+                }}
               >
-                SignUp
+                Already have an account?
               </button>
             </div>
-          </div>
-        </form>
-        <div className="field">
-          <div className="buttons is-right">
-            <button
-              className="button is-ghost"
-              onClick={() => {
-                navigate("/login");
-              }}
-            >
-              Already have an account?
-            </button>
           </div>
         </div>
       </div>
